d8e846a65c0293e8ad56827d2c5a1c0f8fe95c0c
Sagnik Das <dassagnik662@gmail.com>
2025-08-02 23:26:06 +0530

    added a footer

---
+++ frontend/src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-950 w-full text-white text-center text-sm mt-auto py-3">
+    Copyright © 2025 ML Web App. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
+        <div className="flex flex-col">
+          <main className="flex-grow">
+            <Navbar />
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
